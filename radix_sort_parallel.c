5e1aeb356104c2339ef9bb79c78448d96a702fd0
Nicolas Alejandro Di Risio <nicolas@dirisio.net>
2012-01-13 19:30:54 +0100

    Improved parallel version, added parameters, fucking hot version

--- radix_sort_parallel.c
+++ radix_sort_parallel.c
@@ -5,131 +5,129 @@
 #include <omp.h>
 
 #define DIGITS_AMOUNT 	10    /* number of all possible keys (bucket's size) */
-#define DEBUG 		 0
 
-/* max number of digits of the elements' list (calculated) */
-int maxlen = 0;
+uint32_t tenPowers[10] = {
+	1,		10,		100,
+	1000,		10000,		100000,
+	1000000,	10000000,	100000000,
+	1000000000,
+};
 
-/* calculate power */
-uint32_t p(uint32_t base, uint32_t exp)
+/*
+ * Gives power of 10 at certain exp
+ */
+uint32_t p(int exp)
 {
-	return (exp==0 ? 1 : p(base, exp-1)*base);
+	return tenPowers[exp];
 }
 
 /*
- * Radix sorting
+ * Parallel Radix sorting
  */
-void radixsort(uint32_t *in, uint32_t n)
+void radixsort_parallel(uint32_t *input_array, uint32_t input_array_size,
+				uint32_t max_value, int debug, int chunksize)
 {
-	uint32_t digit, t, max=0;
-	int i, j;
+	uint32_t digit, tmp_value;
+	int f, i, j;
+	int max_lenght=0;
+	double start_time;
 
-	uint32_t *tmp = (uint32_t *)calloc(n, sizeof(uint32_t));
-
+	/* allocate temporary array */
+	uint32_t *tmp = (uint32_t *)calloc(input_array_size, sizeof(uint32_t));
 	if (!tmp) {
 		fprintf(stderr, "ERROR: not enough memory!\n");
 		abort();
 	}
 
-	/* 0.1) caclculate max */
-	#pragma omp parallel for private(i)
-	for (i=0; i<n; i++) {
-		if (in[i]>max) {
-			#pragma omp critical
-			max=in[i];
+        /* 1) it calculates max number of digit in max_value [K] */
+	if (debug > 0) {
+		start_time = omp_get_wtime();
+	}
+        tmp_value = max_value;
+        while((tmp_value/=10)>0)
+                max_lenght++;
+        max_lenght++;
+	if (debug > 0) {
+		printf("  1) CALCULATION OF K\t\t\t%lf\n",
+						    omp_get_wtime()-start_time);
+	}
+
+	/* Shared buckets definition */
+	if (debug > 0)
+		start_time = omp_get_wtime();
+	int shared_buckets[max_lenght][DIGITS_AMOUNT];
+	for (j=0; j<max_lenght; j++) {
+		for(f=0; f<DIGITS_AMOUNT; f++)
+			shared_buckets[j][f]=0;
+	}
+
+	/* Least Significant Digit */
+	#pragma omp parallel for private(digit, j, i, f) schedule(dynamic, chunksize)
+	for (i=0; i<input_array_size; i++) {
+
+		/* each thread make these step to its pool of values */
+		for (j=0; j<max_lenght; j++) {
+			digit = p(j);
+
+			/* 2) each bucket is how many times that digit is read
+			 *    so we put in shared bucket all infos
+			 */
+			f=input_array[i]/digit%DIGITS_AMOUNT;
+			#pragma omp atomic
+			shared_buckets[j][f]++;
 		}
 	}
-	#if (DEBUG>1)
-	printf("END MAX @ %lf\n", omp_get_wtime());
-	#endif
-
-	/* 0.2) calculate max number of digit [K] */
-	t = max;
-	while((t/=10) >0)
-		maxlen++;
-	maxlen++;
-
-	#if (DEBUG>1)
-	printf("END MAXLEN @ %lf\n", omp_get_wtime());
-	#endif
-
-	/* 0.3) allocate bucket and init to 0 */
-	int **bucket;
-    	bucket = (int **)calloc(maxlen, sizeof(int *));
-
-	if (!bucket) {
-		fprintf(stderr, "ERROR: not enough memory!\n");
-		abort();
-	}
-
-	for(i=0; i<maxlen+1; i++) {
-        	*(bucket + i) = (int *)calloc(DIGITS_AMOUNT, sizeof(int));
-		if (!*(bucket +i)) {
-			fprintf(stderr, "ERROR: not enough memory!\n");
-			abort();
-		}
-	}
-
-	//max [10][10]
-	for(i=0; i<maxlen; i++) {
-		for(j=0; j<DIGITS_AMOUNT; j++)
-			bucket[i][j] = 0;
-	}
-	#if (DEBUG>1)
-	printf("END BUCKET INIT  @ %lf\n", omp_get_wtime());
-	#endif
-
-	/* 2) each bucket is how many times that digit is read */
-	#pragma omp parallel for private(i, digit) schedule(static)
-	for (i=0; i<maxlen; i++) {
-		digit = p(10,i);
-
-		#pragma omp parallel for private(j) schedule(static)
-		for (j=0; j<n; j++) {
-			bucket[i][in[j]/digit%DIGITS_AMOUNT]++;
-		}
+	if (debug > 0) {
+		printf("  2) CREATION OF BUCKETS\t\t%lf\n",
+						    omp_get_wtime()-start_time);
 	}
 
 	/* 3) sum precedent bucket */
-	for(i=1; i<DIGITS_AMOUNT; i++) {
-		for(j=0; j<maxlen; j++)
-			bucket[j][i]+=bucket[j][i-1];
+	if (debug > 0)
+		start_time = omp_get_wtime();
+	for (j=0; j<max_lenght; j++) {
+		for (i=1; i<DIGITS_AMOUNT; i++)
+			shared_buckets[j][i]+=shared_buckets[j][i-1];
 	}
-
-	#if (DEBUG>1)
-		printf("\t[0]\t[1]\t\[2]\t[3]\t[4]\t[5]\t[6]\t[7]\t[8]\t[9]\n");
-		for (i=0; i<maxlen; i++) {
-			printf("[%i]\t", i);
-			for (j=0; j<DIGITS_AMOUNT; j++)
-				printf(" %i\t", bucket[i][j]);
+	if (debug > 2) {
+		printf("   [0]\t[1]\t\[2]\t[3]\t[4]\t[5]\t[6]\t[7]\t[8]\t[9]\n");
+		for (j=0; j<max_lenght; j++) {
+			printf("%i  ", j);
+			for (i=0; i<DIGITS_AMOUNT; i++)
+				printf(" %i\t", shared_buckets[j][i]);
 			printf("\n");
 		}
-		printf("\n");
-	#endif
+	}
+	if (debug > 0) {
+		printf("  3) SUMMING BUCKETS\t\t\t%lf\n",
+						    omp_get_wtime()-start_time);
+	}
 
-	for(j=0; j<maxlen; j++) {
-		digit = p(10,j);
+	if (debug > 0)
+		start_time = omp_get_wtime();
+	for (j=0; j<max_lenght; j++) {
+		digit = p(j);
 
-		/* 4) order tmp list as bucket-array says */
-		#pragma omp parallel for ordered private(i) schedule(static)
-		for(i=n-1;i>=0;i--) {
-			#pragma omp ordered
-			tmp[--bucket[j][in[i]/digit%DIGITS_AMOUNT]]=in[i];
+		/* 4) order tmp list as shared buckets array says */
+		for(i=input_array_size-1;i>=0;i--) {
+			int unit = input_array[i]/digit%DIGITS_AMOUNT;
+			int pos = --shared_buckets[j][unit];
+			tmp[pos]=input_array[i];
 		}
-
 		/* 5) copy into input list */
-		#pragma omp parallel for private(i) schedule(static)
-		for(i=0;i<n;i++)
-			in[i]=tmp[i];
-
-		#if (DEBUG>1)
+		for(i=0;i<input_array_size;i++)
+			input_array[i]=tmp[i];
+		if (debug > 2) {
 			char layout[80];
-			int c;
-			sprintf(layout, "%%%iu\n", maxlen+1);
-			for (c=0; c<n; c++)
-				printf(layout, in[c]);
-			printf(" ***\n");
-		#endif
+			sprintf(layout, "%%%iu\n", max_lenght+1);
+			for (i=0; i<input_array_size; i++)
+				printf(layout, input_array[i]);
+			printf("***\n");
+		}
+	}
+	if (debug > 0) {
+		printf("4-5) USING BUCKETS AND COPYING\t\t%lf\n",
+						    omp_get_wtime()-start_time);
 	}
 
 	cfree(tmp);
@@ -140,80 +138,119 @@ void radixsort(uint32_t *in, uint32_t n)
  */
 int main(int argc, char **argv)
 {
-	uint32_t i, *input, n = 1000;
-	int k=5;
-
+	uint32_t i, *input, num_of_values = 1000, max_value = 0;
+	int value_lenght=5;
 	double start_time;
 	char str[10] = {0};
+	int chunksize=1;
+	int debug=0;
+	char layout[80];
 
 	/*
 	 *  1st opt param size of input list (default 1000)		[N]
 	 *  2nd opt param max digits of the elements' list (default 5)	[K]
+	 *  3rd opt debug level(num) (default level 0)
+	 *  4rd opt chunk size (default is 1)
 	 */
 	if (argc >= 2) {
-		n = strtoul(argv[1], NULL, 10);
-		if (n < 0 || n > UINT32_MAX)
-			n = 1000;
+		num_of_values = strtoul(argv[1], NULL, 10);
+		if (num_of_values < 0 || num_of_values > UINT32_MAX)
+			num_of_values = 1000;
 	}
 	if (argc >= 3) {
-		k = atoi(argv[2]);
-		if (k < 0 || k > 10 )
-			k = 5;
-		for(i=0;i<k;i++)
-			str[i] = '9';
+		value_lenght = atoi(argv[2]);
+		if (value_lenght < 0 || value_lenght > 10 )
+			value_lenght = 5;
+	}
+	if (argc >= 4) {
+		int tmp_value;
+		if ((tmp_value=atoi(argv[3]))>0) {
+			debug=tmp_value;
+		}
+	}
+	if (argc >= 5) {
+		int tmp_value;
+		if ((tmp_value=atoi(argv[4]))>0) {
+			chunksize=tmp_value;
+		}
 	}
 
-	#if (DEBUG>0)
-		printf("Debug Level: %i\n", DEBUG);
-	#endif
+        if (debug > 0) {
+		printf("DEBUG LEVEL: %i\n", debug);
+		printf("CHUNKSIZE  : %i\n", chunksize);
+	}
 
 	/* allocation of memory */
-	input = (uint32_t *)calloc(n, sizeof(uint32_t));
+	if (debug > 0)
+		start_time = omp_get_wtime();
+	input = (uint32_t *)calloc(num_of_values, sizeof(uint32_t));
 	if (!input) {
 		fprintf(stderr, "ERROR: not enough memory!\n");
 		return 1;
 	}
+	if (debug > 0) {
+		printf("INPUT ALLOCATION\t\t\t%lf\n", omp_get_wtime() -
+								    start_time);
+	}
 
 	/* generate random input list */
-	#if (DEBUG>0)
-		printf("Random Creation\n");
-	#endif
-	srand(0);
-	for(i=0;i<n; i++)
+	for(i=0;i<value_lenght;i++)
+		str[i] = '9';
+	if (debug > 0)
+		start_time = omp_get_wtime();
+	srand(time(NULL));
+	for(i=0;i<num_of_values; i++) {
 		input[i] = rand() % strtoul(str,NULL,10) + 1;
+		if (max_value < input[i])
+			max_value = input[i];
+	}
+	if (debug > 0) {
+		printf("RANDOM GENERATION\t\t\t%lf\n", omp_get_wtime() -
+								    start_time);
+	}
 
 	/* print unordered input list */
-	#if (DEBUG>0)
-		char layout[80];
-		sprintf(layout, "%%%iu\n", k+1);
-		for (i=0; i<n; i++)
+	if (debug > 1) {
+		sprintf(layout, "%%%iu\n", value_lenght+1);
+		for (i=0; i<num_of_values; i++)
 			printf(layout, input[i]);
-	#endif
+	}
 
 	/* sort input list */
-	#if (DEBUG>0)
-		printf("Radix Sorting\n");
-	#endif
+	if (debug > 0)
+		printf("START RADIX SORTING\n");
 	start_time = omp_get_wtime();
-	radixsort(input, n);
-	#if (DEBUG>0)
+	radixsort_parallel(input, num_of_values, max_value, debug, chunksize);
+	if (debug > 0)
 		printf("K\t\tN\t\tTIME\n");
-	#endif
-	printf("%i\t%8u\t\t%f\n", maxlen, n, omp_get_wtime()-start_time);
+	printf("%i\t%8u\t\t%f\n", value_lenght, num_of_values,
+						  omp_get_wtime() - start_time);
 
 	/* print ordered input list */
-	#if (DEBUG>0)
-		sprintf(layout, "%%%iu\n", maxlen+1);
-		printf(layout, input[0]);
-		for (i=1; i<n; i++) {
+	if (debug > 1) {
+		sprintf(layout, "%%%iu\n", value_lenght+1);
+		for (i=0; i<num_of_values; i++)
 			printf(layout, input[i]);
-			if (input[i]<input[i-1])
-				printf("ERRORE\n");
+	}
+
+	/* check values order */
+	int not_ordered = 0;
+	for (i=1; i<num_of_values; i++) {
+		if (input[i]<input[i-1]) {
+			not_ordered = 1;
+			break;
 		}
-	#endif
+	}
+	if (not_ordered) {
+		printf("ORDERED CHECK: FAIL!\n");
+	} else {
+		if (debug > 0)
+			printf("ORDERED CHECK: PASS\n");
+	}
 
 	/* free memory */
 	cfree(input);
 
 	return 0;
 }
+
